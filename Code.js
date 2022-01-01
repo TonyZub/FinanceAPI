7c03ca55b002605efcc4a39ffc5636901230d73c
Anton Zubkov <zub0296@mail.ru>
2022-01-01 18:28:38 +0700

    Added base files

---
+++ Code.js
@@ -0,0 +1,59 @@
+// #region Constants
+
+const REQ_URL = "https://script.google.com/macros/s/AKfycbzHcmNqLO4qUYBl7A0C6Me39Fmhvz3ibQzzagtq6YwtDj0-ZfdtVWU9sI1_5qC3wrop/exec";
+const NOT_FOUND = "/404"
+
+// #endregion
+
+
+// #region Fields
+
+var cookies = GetCookies();
+
+// #endregion
+
+
+// #region Functions - Cookies
+
+function GetCookies(){
+  return document.cookie
+    .split(';')
+    .map(cookie => cookie.split('='))
+    .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
+}
+
+function SetCookie(name, value){
+  if(name != "") document.cookie = name + "=" + (value || "");
+}
+
+// #endregion
+
+
+// #region Request functions
+
+function MakeRequest(requestType, functionName, parameter){
+  let xhr = new XMLHttpRequest();
+    xhr.open(requestType, REQ_URL);
+    xhr.send(JSON.stringify(new Object({token: cookies["Token"], function: functionName, paramter: parameter})));   
+  return xhr;
+}
+
+function CheckToken(){
+  let request = MakeRequest("GET", "CheckToken");
+  request.onload = function() {
+    if (request.status != 200) {
+      alert("status error");
+      // connection error - request status error
+    } 
+    else {
+      let responseJSON = JSON.parse(xhr.response);
+      console.log(responseJSON);
+      alert("success");
+    };
+  };
+  request.onerror = function(request) {
+    alert("connection error");
+  };
+}
+
+// #endregion
